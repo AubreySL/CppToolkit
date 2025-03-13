785da9c94914e06d62f338930c8065ce865f5d12
AubreySL <soligu@outlook.com>
2025-03-13 18:58:56 +0800

    feat: add dll main;

--- 05-win_api/RemoteInjectDll/main.cpp
+++ 05-win_api/RemoteInjectDll/main.cpp
@@ -7,6 +7,50 @@ BOOL APIENTRY DllMain(HMODULE hModule,
     LPVOID lpReserved
 )
 {
+    TCHAR szbuff[MAX_PATH] = {0};
+    LPBYTE lpAddress = NULL;
+    MEMORY_BASIC_INFORMATION mbi = { 0 };//page info
+    int nLen;
+    TCHAR szModName[MAX_PATH] = { 0 };
+    HWND hwndRemoteApp;
 
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        //find target window
+        hwndRemoteApp = FindWindow(TEXT(""), TEXT("RemoteApp"));
+        
+        while (VirtualQuery(lpAddress, &mbi, sizeof(mbi)) == sizeof(mbi))
+        {
+            //virtual page state is free
+            if (mbi.State == MEM_FREE)
+            {
+                mbi.AllocationBase = mbi.BaseAddress;
+            }
+            if ((mbi.AllocationBase == NULL) || (mbi.AllocationBase == hModule) ||
+                (mbi.BaseAddress != mbi.AllocationBase))
+            {
+                nLen = 0;
+            }
+            else {
+                nLen = GetModuleFileName(HMODULE(mbi.AllocationBase), szModName, _countof(szModName));
+            }
 
+            if (nLen)
+            {
+                wsprintf(szbuff, TEXT("%p\t%s\r\n"), mbi.AllocationBase, szModName);
+                //edit compoent show module name
+                SendDlgItemMessage(hwndRemoteApp, 1005, EM_SETSEL, -1, -1);
+                SendDlgItemMessage(hwndRemoteApp, 1005, EM_REPLACESEL, TRUE, (LPARAM)szbuff);
+            }
+            lpAddress += mbi.RegionSize;
+        }
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+    default:
+        break;
+    }
+    return true;
 }
