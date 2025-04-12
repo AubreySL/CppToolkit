9cec63a8d7841f49ba9986a316bbc4b9a4a4300d
AubreySL <soligu@outlook.com>
2025-04-12 18:53:52 +0800

    feat: cpp 20 sample;

---
+++ 07-cpp20/sample/Employee.cpp
@@ -0,0 +1,18 @@
+module employee;
+import <iostream>;
+import <format>;
+using namespace std;
+
+namespace Records {
+	//Employee::Employee(const std::string& firstName, const std::string& lastName)
+	//	:m_firstName{ firstName }, m_lastName{ lastName }
+	//{
+	//}
+
+	//void Employee::promote(int raiseAmount)
+	//{
+	//}
+
+	
+
+}
